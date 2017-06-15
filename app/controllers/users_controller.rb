936a00972885601df959ef64430576f13e09e223
Bekzhan <bkalzhan@gmail.com>
2017-06-15 07:49:07 +0000

    Insta without error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,18 @@ class UsersController < ApplicationController
     end
     
     def create
-        @user = User.new(params[:user])
+        @user = User.new user_params
         if @user.save
             redirect_to root_path
         else
             render "new"
         end
-        
-        private
-        
-        def user_params
-           params.require(:user).permit(:login, :email, :password) 
-        end
+    end
+    
+    
+    private
+    
+    def user_params
+       params.require(:user).permit(:login, :email, :password) 
     end
 end
